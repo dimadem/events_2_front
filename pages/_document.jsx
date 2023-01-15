a15ed5f7ebb19f430a6246c5e0bce7a578bb0de5
EwRvp7LV7 <36769374+EwRvp7LV7@users.noreply.github.com>
2023-01-15 16:50:26 +0700

    final (#5)
    * table fixed. Confirmation aded
    
    * todo pointed
    
    * Create nextjs.yml
    
    * Create codeql.yml
    
    * final

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="fixed top-0 ">
+      <body className="top-0">
         <div className=" w-screen h-screen p-8 bg-gradient-to-r from-slate-300 via-slate-400 to-slate-500 overflow-y-auto">
           <Main />
           <NextScript />
